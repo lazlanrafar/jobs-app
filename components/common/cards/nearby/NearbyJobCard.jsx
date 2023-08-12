7e30d3bafa6a3e4e5a0a3fcccec8436246f6f73f
lazlanrafar <lazlanrafar@gmail.com>
2023-08-12 20:51:26 +0700

    slicing nearby job section

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-import styles from './nearbyjobcard.style'
+import styles from "./nearbyjobcard.style";
+import { checkImageURL } from "../../../../utils";
 
-const NearbyJobCard = () => {
+const NearbyJobCard = ({ selectedJob, item, handleCardPress }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode="contain"
+          style={styles.logImage}
+        />
+      </TouchableOpacity>
 
-export default NearbyJobCard
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.jobType} numberOfLines={1}>
+          {item.job_employment_type}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NearbyJobCard;
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 
-import styles from './nearbyjobs.style'
+import styles from "./nearbyjobs.style";
+import { useRouter } from "expo-router";
+import useFetch from "../../../hook/useFetch";
+import { COLORS, SIZES } from "../../../constants";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Nearbyjobs
+  const { data, isLoading, error } = useFetch("search", {
+    query: "Python developer in Texas, USA",
+    num_pages: 1,
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>View all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data.data}
+            renderItem={({ item }) => (
+              <NearbyJobCard
+                item={item}
+                handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+              />
+            )}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ rowGap: SIZES.small }}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
